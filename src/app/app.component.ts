d6a7a58b34e829291b6195a1061efe70ebeeb642
Manisha <manisha@codeest.com>
2019-11-15 16:01:37 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { User } from './user';
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   topics=['Angular','React','Vue']
   topicHasError=true;
-  userModel=new User ("Rob","Rob@gmail.com",4543266,"default","morning",true);
+  userModel=new User ("Rob","Rob@gmail.com",4543266567,"default","morning",true);
+  submitted=false;
+  errorMsg='';
+constructor(private _enrollmentService:EnrollmentService) {
+ 
 
-  validateTopic(value){
-if(value=="default")
-{
-  this.topicHasError=true;
-}else
-{
-  this.topicHasError=false;
-}
+} 
+  validateTopic(value)
+  {
+   if(value=="default")
+   {
+      this.topicHasError=true;
+   } else
+   {
+       this.topicHasError=false;
+   }
   }
+  onSubmit(){
+    this.submitted=true;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(data=>console.log('Success',data),
+               error=>this.errorMsg=error.statusText
+               
+   );
+    
+    
+  }
+ 
   }
