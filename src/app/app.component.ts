96ef7b18dd99010a1d5313c86d62149b72d4d3d9
Manisha <manisha@codeest.com>
2019-11-15 14:46:10 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { User } from './user';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics=['Angular','React','Vue']
+  topicHasError=true;
+  userModel=new User ("Rob","Rob@gmail.com",4543266,"default","morning",true);
+
+  validateTopic(value){
+if(value=="default")
+{
+  this.topicHasError=true;
+}else
+{
+  this.topicHasError=false;
 }
+  }
+  }
